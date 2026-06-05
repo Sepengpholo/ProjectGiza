d317c978f75eee6b94ba6fdfb983434d28e8d6fc
Sepengpholo <166597085+Sepengpholo@users.noreply.github.com>
2026-06-05 13:11:27 +0200

    Update and rename requirements.txt to scripts/mesh_discovery.py

---
+++ scripts/mesh_discovery.py
@@ -0,0 +1,15 @@
+import requests, socket, time
+GIST_ID = 8b44dc1fca767767acc448045c9025b7
+TOKEN = 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
+
+def update_gist(ip):
+    data = {"files": {"nodes.txt": {"content": ip}}}
+    requests.patch(f"https://api.github.com/gists/{GIST_ID}", json=data, 
+                   headers={"Authorization": f"token {TOKEN}"})
+
+while True:
+    try:
+        ip = socket.gethostbyname(socket.gethostname())
+        update_gist(ip)
+    except: pass
+    time.sleep(60)
