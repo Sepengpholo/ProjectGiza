9ad44b20fac220cc2ba1dc4b71eb8121ff2c2574
Sepengpholo <166597085+Sepengpholo@users.noreply.github.com>
2026-06-05 13:48:50 +0200

    Update mesh_discovery.py

--- scripts/mesh_discovery.py
+++ scripts/mesh_discovery.py
@@ -1,11 +1,13 @@
 import requests, socket, time
+
+# Replace with your Gist ID and Token
 GIST_ID = 8b44dc1fca767767acc448045c9025b7
 TOKEN = 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
 
 def update_gist(ip):
     data = {"files": {"nodes.txt": {"content": ip}}}
-    requests.patch(f"https://api.github.com/gists/{GIST_ID}", json=data, 
-                   headers={"Authorization": f"token {TOKEN}"})
+    headers = {"Authorization": f"token {TOKEN}"}
+    requests.patch(f"https://api.github.com/gists/{GIST_ID}", json=data, headers=headers)
 
 while True:
     try:
@@ -13,3 +15,4 @@ while True:
         update_gist(ip)
     except: pass
     time.sleep(60)
+
