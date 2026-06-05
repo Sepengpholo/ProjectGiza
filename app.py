2647e9cf9a34ac1c53a77840d7c2ad9d4f905c80
Sepengpholo <166597085+Sepengpholo@users.noreply.github.com>
2026-06-05 13:40:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,15 +8,14 @@ def home():
     return '''
     <h1>Project Giza Controller</h1>
     <form action="/run" method="post">
-        <textarea name="cmd" rows="5" cols="40"></textarea><br>
-        <input type="submit" value="Send Command to Swarm">
+        <textarea name="cmd" rows="5" cols="40" placeholder="Enter command..."></textarea><br>
+        <input type="submit" value="Execute Command">
     </form>
     '''
 
 @app.route('/run', methods=['POST'])
 def run():
     command = request.form['cmd']
-    # This runs the command on your host
     output = subprocess.check_output(command, shell=True).decode()
     return f"<pre>{output}</pre><a href='/'>Go Back</a>"
 
